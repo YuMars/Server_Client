f1d647d3ee3fe1cdcefb9ab584f29cc0a7c14e9a
Red-Fish <365795976@qq.com>
2023-07-28 16:20:07 +0800

    init

---
+++ Swift_Client/Swift_Client/Login/LoginVC.swift
@@ -0,0 +1,18 @@
+//
+//  LoginVC.swift
+//  Swift_Client
+//
+//  Created by Red-Fish on 2023/7/28.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ Swift_Client/Swift_Client/Register/RegisterVC.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterVC.swift
+//  Swift_Client
+//
+//  Created by Red-Fish on 2023/7/28.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
